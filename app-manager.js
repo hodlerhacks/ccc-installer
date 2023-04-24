c58f905cde066c65a824fa4aeb5da64bb9fcc09b
hodlerhacks <leon@hodlerhacks.com>
2023-04-24 21:51:22 +0200

    :rocket: ccc-installer

---
+++ app-manager.js
@@ -0,0 +1,142 @@
+'use strict';
+
+const TelegramBot = require('node-telegram-bot-api');
+const configFile = 'config.json';
+const config = require(`./${configFile}`);
+const execSync = require('child_process').execSync;
+const fs = require('fs');
+
+const apppath = '/var/opt/';
+let tgBot;
+
+initTelegram();
+
+function initTelegram() {
+    tgBot = new TelegramBot(config.telegramToken, { polling: true });
+
+    tgBot.onText(/\/app/, async ctx => {
+        if (!validateTelegram(ctx)) return;
+        tgBot.removeListener("callback_query");
+
+        const apps = fetchApplications();
+
+        if (apps.length == 0) {
+            tgBot.sendMessage(ctx.chat.id, `No apps installed`).catch((err) => { telegramError(err) });
+        } else {
+            let keyboard = [];
+            let keyboardRow = [];
+            apps.forEach((app, i) => {
+                keyboardRow = [];
+                keyboardRow.push({
+                    text: app,
+                    callback_data: app
+                });
+                keyboard[i] = keyboardRow;
+            });
+            tgBot.sendMessage(ctx.chat.id, `Which app do you want to manage?`, {
+                reply_markup: {
+                    inline_keyboard: keyboard
+                }
+            }).catch((err) => { telegramError(err) });
+        }
+
+        tgBot.on("callback_query", async (callbackQuery) => {
+            const selectedApp = callbackQuery.data;
+            const ctx = callbackQuery.message;
+            tgBot.answerCallbackQuery(callbackQuery.id).then(async () => {
+                tgBot.sendMessage(ctx.chat.id, `What to do with your application?`, {
+                    reply_markup: {
+                        inline_keyboard: [
+                            [
+                                {
+                                    text: "Start",
+                                    callback_data: 'start'
+                                },
+                                {
+                                    text: "Stop",
+                                    callback_data: 'stop'
+                                },
+                                {
+                                    text: "Restart",
+                                    callback_data: 'restart'
+                                },
+                                {
+                                    text: "Install",
+                                    callback_data: 'install'
+                                },
+                            ],
+                        ]
+                    }
+                }).catch((e) => { console.log(e) });
+                tgBot.on("callback_query", async (callbackQuery) => {
+                    const selection = callbackQuery.data;
+                    const ctx = callbackQuery.message;
+                    tgBot.answerCallbackQuery(callbackQuery.id).then(async () => {
+                        if (selection == 'start') {
+                            // Get entry point from package.json
+                            const json = await getJson(apppath + selectedApp + '/package.json');
+                            const main = json.main;
+                            const mainExists = fs.existsSync(apppath + selectedApp + '/' + main);
+                            if (!main) {
+                                await tgBot.sendMessage(ctx.chat.id, `No main file specified in package.json`).catch((e) => { console.log(e) });
+                            } else if (!mainExists) {
+                                await tgBot.sendMessage(ctx.chat.id, `The main file specified in package.json does not exist`).catch((e) => { console.log(e) });
+                            } else {
+                                await tgBot.sendMessage(ctx.chat.id, `Starting application... please wait`).catch((e) => { console.log(e) });
+                                // First stop to avoid duplicate processes running, '|| true' to avoid errors in case no process exists yet
+                                execSync(`pm2 stop ${selectedApp} || true`, { stdio: 'inherit', cwd: apppath + selectedApp });
+                                execSync(`pm2 start "${main}" --name="${selectedApp}"`, { stdio: 'inherit', cwd: apppath + selectedApp });
+                                execSync(`pm2 save`, { stdio: 'inherit', cwd: apppath + selectedApp });
+                                await tgBot.sendMessage(ctx.chat.id, `Application started`).catch((e) => { console.log(e) });
+                            }
+                        }
+                        if (selection == 'stop') {
+                            await tgBot.sendMessage(ctx.chat.id, `Stopping application... please wait`).catch((e) => { console.log(e) });
+                            execSync(`pm2 stop ${selectedApp}`, { stdio: 'inherit', cwd: apppath + selectedApp });
+                            await tgBot.sendMessage(ctx.chat.id, `Application stopped`).catch((e) => { console.log(e) });
+                        }
+                        if (selection == 'restart') {
+                            await tgBot.sendMessage(ctx.chat.id, `Restarting application... please wait`).catch((e) => { console.log(e) });
+                            execSync(`pm2 restart ${selectedApp}`, { stdio: 'inherit', cwd: apppath + selectedApp });
+                            await tgBot.sendMessage(ctx.chat.id, `Application restarted`).catch((e) => { console.log(e) });
+                        }
+                        if (selection == 'install') {
+                            await tgBot.sendMessage(ctx.chat.id, `Installing application... please wait`).catch((e) => { console.log(e) });
+                            execSync(`npm install`, { stdio: 'inherit', cwd: apppath + selectedApp });
+                            await tgBot.sendMessage(ctx.chat.id, `Installation completed`).catch((e) => { console.log(e) });
+                        }
+                    });
+                    tgBot.removeListener("callback_query");
+                });
+            });
+            tgBot.removeListener("callback_query");
+        });
+    });
+}
+
+function validateTelegram(ctx) {
+    if (ctx.from.username != config.telegramUsername && '@' + ctx.from.username != config.telegramUsername) {
+        tgBot.sendMessage(ctx.chat.id, `Unauthorized user`).catch((err) => { console.log(e) });
+        return false;
+    }
+    return true;
+}
+
+function fetchApplications() {
+    let apps = [];
+    fs.readdirSync(apppath, { withFileTypes: true }).forEach((entry) => {
+        if (entry.isDirectory() && entry.name != 'ccc-installer') apps.push(entry.name);
+    });
+    return apps;
+}
+
+async function getJson(file) {
+    let json;
+    try {
+        const data = fs.readFileSync(file, {encoding: 'utf8'});
+        json = JSON.parse(data);
+    } catch (err) {
+        console.log(`Couldn't read JSON file:`, err);
+    }
+    return json;
+}
---
+++ install-server.sh
@@ -0,0 +1,231 @@
+##################################################################################
+#!/bin/bash
+# To install: wget -P /tmp -L https://raw.githubusercontent.com/hodlerhacks/ccc-installer/master/install-server.sh install-server.sh; bash /tmp/install-server.sh
+##################################################################################
+VERSION="1.0.0"
+APPPATH=/var/opt
+INSTALLERFOLDER=ccc-installer
+APPSCRIPTREPOSITORY=https://github.com/hodlerhacks/ccc-installer.git
+APPMANAGER=app-manager.js
+##################################################################################
+bashrc_shortcuts=(installer)
+installer="'bash "$APPPATH"/"$INSTALLERFOLDER"/install-server.sh'"
+##################################################################################
+
+write_bashrc_shortcut() {
+	all_args=("$@")
+	rest_args=("${all_args[@]:1}")
+	shortcut_cmd="${rest_args[@]}"
+
+ 	sed -i "s|^alias $1.*|alias $1=$shortcut_cmd|gI" ~/.bashrc
+}
+
+update_bashrc_shortcuts() {
+	for i in "${bashrc_shortcuts[@]}"
+		do
+			eval shortcut_string=\$$i
+			write_bashrc_shortcut $i $shortcut_string
+	done
+}
+
+check_bashrc_shortcuts() {
+	for i in "${bashrc_shortcuts[@]}"
+		do	
+			if grep -q $i ~/.bashrc; then
+				echo "Alias "$i" exist"
+				update_bashrc_shortcuts
+			else	
+				eval shortcut_string=\$$i
+				echo "Alias does not exist!!"
+				echo "alias "$i"="$shortcut_string >> ~/.bashrc
+			fi
+	done
+}
+
+check_installation() {
+# If node_modules is not available
+if [ ! -d "$APPPATH"/"$INSTALLERFOLDER"/node_modules ]; then
+	clear
+	echo ""
+	echo "  Application Manager has not been installed yet"
+	echo ""
+	echo "  Starting installation..."
+	echo ""
+
+	press_enter
+	app_install
+fi
+}
+
+start_app() { 
+	check_installation
+
+	cd "$APPPATH"/"$INSTALLERFOLDER"
+	# First stop to avoid duplicate processes running, '|| true' to avoid errors in case no process exists yet
+	pm2 stop App_Manager || true
+    pm2 start "app-manager.js" --name="App_Manager"
+	pm2 save
+}
+
+restart_app() { 
+	check_installation
+
+	pm2 restart App_Manager
+}
+
+stop_app() { 
+	check_installation
+
+	pm2 stop App_Manager
+}
+
+app_install() {
+	script_install
+	npm install
+
+	# If config.json does not exist, create it
+	if [ ! -f "$APPPATH"/"$INSTALLERFOLDER"/config.json ]; then
+		configure_telegram
+		clear
+    fi
+
+	start_app
+	script_refresh
+}
+
+script_install() {
+	# Save config files
+	if [ -f "$APPPATH"/"$INSTALLERFOLDER"/config.json ]; then
+		mkdir /tmp/
+		cp -a "$APPPATH"/"$INSTALLERFOLDER"/config.json /tmp/
+    fi
+
+	if [ -d "$APPPATH"/"$INSTALLERFOLDER"/.git ]; then
+	# If local repository exists check for updates		
+		cd "$APPPATH"/"$INSTALLERFOLDER"
+			git pull --ff-only origin master
+	else
+		git clone "$APPSCRIPTREPOSITORY" "$APPPATH"/"$INSTALLERFOLDER"
+	fi
+
+	# Recover config files
+	if [ -f /tmp/config.json ]; then
+		cp -a /tmp/config.json "$APPPATH"/"$INSTALLERFOLDER"/config.json
+		rm -r /tmp
+    fi
+
+	check_bashrc_shortcuts
+}
+
+script_update() {
+	script_install
+	check_bashrc_shortcuts
+
+	if [ -f /tmp/install-server.sh ]; then
+		rm -r /tmp/install-server.sh
+		script_refresh
+	fi
+}
+
+script_refresh() {
+	/bin/bash "$APPPATH"/"$INSTALLERFOLDER"/install-server.sh
+}
+
+reload_shell() {
+	cd
+	exec bash
+}
+
+press_enter() {
+	echo ""
+  	echo -n "  Press Enter to continue "
+  	read
+  	clear
+}
+
+configure_telegram() {
+	clear
+	echo ""
+	echo "  Create a new Telegram bot (unique for this application) and enter its details below"
+	echo ""
+
+	echo ""
+	echo -n "  > Enter Telegram username: "
+	read username
+
+	echo ""
+	echo -n "  > Enter Telegram token: "
+	read token
+
+	echo "{\"telegramUsername\":\"${username}\",\"telegramToken\":\"${token}\"}" > config.json
+}
+
+server_install() { 
+	pm2 delete all
+
+	# Install packages
+	echo "### Installing packages ###"
+	apt -y update
+	apt -y install git
+	apt -y install -y nodejs
+	apt -y install npm
+	npm install pm2@latest -g
+	apt -y update
+
+	# Set maximum pm2 log file size and number of rotate files
+	pm2 install pm2-logrotate
+	pm2 set pm2-logrotate:max_size 10M
+	pm2 set pm2-logrotate:retain 2
+	
+	# Create PM2 startup
+	pm2 startup
+}
+
+incorrect_selection() {
+  	echo "Incorrect selection! Try again."
+}
+
+if [[ $EUID -ne 0 ]]; then
+   	echo "This script must be run as root"
+   	exit 1
+fi
+
+script_update
+
+until [ "$selection" = "0" ]; do
+	clear
+	echo "---------------------------------------------------------"
+	echo ""
+	echo "                    Installer v"$VERSION
+	echo ""
+	echo "---------------------------------------------------------"
+	echo ""
+	echo "      1  -  Install Server"
+	echo "      2  -  Install App Manager"
+	echo ""
+	echo "      s  -  Start App Manager"
+	echo "      t  -  Stop App Manager"
+	echo "      r  -  Re-start App Manager"
+	echo "" 
+	echo "      c  -  Change Telegram settings"
+	echo "      u  -  Update Installer"
+	echo ""
+	echo "      0  -  Exit"
+	echo ""
+	echo "---------------------------------------------------------"
+	echo "" 
+	echo -n "  Enter selection: "
+	read selection
+	echo ""
+	case $selection in
+		1 ) clear ; server_install; press_enter ;;
+		2 ) clear ; app_install; press_enter ;;
+		s ) clear ; start_app ; press_enter ;;
+		t ) clear ; stop_app ; press_enter ;;
+		r ) clear ; restart_app ; press_enter ;;
+		c ) clear ; configure_telegram ;;
+		u ) clear ; script_update; press_enter; script_refresh ;;
+		0 ) clear ; reload_shell ;;
+		* ) clear ; incorrect_selection ; press_enter ;;
+	esac
+done
