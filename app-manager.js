90cc012a3b04b858f8ace3fae85ae1f18219ddb8
hodlerhacks <leon@hodlerhacks.com>
2023-05-07 12:01:00 +0200

    ccc-installer

--- app-manager.js
+++ app-manager.js
@@ -95,10 +95,15 @@ function handleAppAction(selectedApp, ctx) {
                 } else {
                     await tgBot.sendMessage(ctx.chat.id, `Starting application... please wait`).catch((e) => { console.log(e) });
                     // First stop to avoid duplicate processes running, '|| true' to avoid errors in case no process exists yet
-                    execSync(`pm2 stop ${selectedApp} || true`, { stdio: 'inherit', cwd: apppath + selectedApp });
-                    execSync(`pm2 start "${main}" --name="${selectedApp}"`, { stdio: 'inherit', cwd: apppath + selectedApp });
-                    execSync(`pm2 save`, { stdio: 'inherit', cwd: apppath + selectedApp });
+                    try {
+                        execSync(`pm2 stop ${selectedApp} || true`, { stdio: 'inherit', cwd: apppath + selectedApp });
+                        const output = execSync(`pm2 start "${main}" --name="${selectedApp}"`, { stdio: 'inherit', cwd: apppath + selectedApp });
+                        execSync(`pm2 save`, { stdio: 'inherit', cwd: apppath + selectedApp });
+                    } catch (e) {
+                        console.log('ERROR:', e)
+                    }
                     await tgBot.sendMessage(ctx.chat.id, `Application started`).catch((e) => { console.log(e) });
+                    await tgBot.sendMessage(ctx.chat.id, output).catch((e) => { console.log(e) });
                 }
             }
             if (selection == 'stop') {
